0744637c2b11c633e597874a2fb647226e71d587
Bakr101 <ahmed98bakr@gmail.com>
2025-06-15 16:49:41 +0300

    init DB

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,21 @@
+import process from "node:process";
+
+process.loadEnvFile();
+function envOrThrow(key: string): string {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Environment variable ${key} is not set`);
+    }
+    return value;
+}
+const dbURL = envOrThrow("DB_URL");
+
 type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
 }
 
 export const config: APIConfig = {
     fileserverHits: 0,
+    dbURL,
 }
